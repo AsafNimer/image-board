a3d592ad0a06652627f662302c7783156498b071
AsafNimer <102157450+AsafNimer@users.noreply.github.com>
2023-06-21 15:55:37 +0200

    image board comments bug fixed

--- public/js/comments-component.js
+++ public/js/comments-component.js
@@ -8,20 +8,29 @@ const commentsComponent = {
     },
     props: ["imgId"],
     mounted() {
-        fetch(`/comments/${this.imgId}`)
-            .then((res) => res.json())
-            .then((fetchedData) => {
-                console.log(
-                    "FETCHED DATA: comments are retreived",
-                    fetchedData
-                );
-                this.comments = fetchedData;
-            })
-            .catch((err) => {
-                console.log("ERROR: PROBLEM TO FETCH DATA ", err);
-            });
+        this.fetchData();
+        console.log("mounted comments", this.imgId);
+    },
+    watch: {
+        imgId: function () {
+            this.fetchData();
+        },
     },
     methods: {
+        fetchData() {
+            fetch(`/comments/${this.imgId}`)
+                .then((res) => res.json())
+                .then((fetchedData) => {
+                    console.log(
+                        "FETCHED DATA: comments are retreived",
+                        fetchedData
+                    );
+                    this.comments = fetchedData;
+                })
+                .catch((err) => {
+                    console.log("ERROR: PROBLEM TO FETCH DATA ", err);
+                });
+        },
         handleCommentSubmit(e) {
             e.preventDefault();
 
